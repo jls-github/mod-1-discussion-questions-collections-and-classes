b12a0b21c845452beec78e92a407c32e66185290
github username <github email address>
2020-03-13 11:44:39 -0700

    group 1 review question 2

--- review-question-2.rb
+++ review-question-2.rb
@@ -1,8 +1,24 @@
 # Finish the implementation of the Car class so it has the functionality described below
+require 'pry'
 
 class Car
+    attr_reader :make, :model
 
+    @@all = []
 
+    def initialize(make, model)
+        @make = make
+        @model = model
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.drive
+        return "VROOOOOOOOOOOOM!"
+    end
 
 end
 
@@ -21,11 +37,12 @@ Car.drive
 Car.all
 #=> [#<Car:0x00007fae28930f20>, #<Car:0x00007fae28923370>, #<Car:0x00007fae2891ae78>]
 
-BONUS:
+#BONUS:
 
-volvo_lightning = Car.new(make: "Volvo", model: "Lightning")
+#volvo_lightning = Car.new(make: "Volvo", model: "Lightning")
 
 volvo_lightning.make
 #=> "Volvo"
 volvo_lightning.model
 #=> "Lightning"
+binding.pry
